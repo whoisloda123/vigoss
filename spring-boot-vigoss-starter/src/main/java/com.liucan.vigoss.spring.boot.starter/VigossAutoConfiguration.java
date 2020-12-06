360bd6589407df0c9063dedf8e771f3b6d58b8d5
can.liu <553670590@qq.com>
2020-12-06 19:22:18 +0800

    feat:second commit

--- spring-boot-vigoss-starter/src/main/java/com.liucan.vigoss.spring.boot.starter/VigossAutoConfiguration.java
+++ spring-boot-vigoss-starter/src/main/java/com.liucan.vigoss.spring.boot.starter/VigossAutoConfiguration.java
@@ -1,6 +1,6 @@
 package com.liucan.vigoss.spring.boot.starter;
 
-import com.liucan.vigoss.core.VigossConnnector;
+import com.liucan.vigoss.core.VigossConnector;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,17 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 统一注入到bean里面
  * @author liucan
  * @date 12/6/20 3:42 PM
  */
 @Configuration
 @ConditionalOnClass(VigossProperties.class)
+//@AutoConfigureAfter(GsonAutoConfiguration.class)
 @EnableConfigurationProperties(VigossProperties.class)
 public class VigossAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(name = "vigossConnnector")
-    public VigossConnnector vigossConnnector() {
-        return new VigossConnnector();
+    public VigossConnector vigossConnnector() {
+        return new VigossConnector();
     }
 }
--- vigoss-core/src/main/java/com.liucan.vigoss.core/VigossConnector.java
+++ vigoss-core/src/main/java/com.liucan.vigoss.core/VigossConnector.java
@@ -4,5 +4,5 @@ package com.liucan.vigoss.core;
  * @author liucan
  * @date 12/6/20 3:44 PM
  */
-public class VigossConnnector {
+public class VigossConnector {
 }
